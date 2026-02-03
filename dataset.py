b9e869e0a0e3fed7ec6e5e5201ef8745f7fab97c
Fatim Kone <s2273998@hastings.inf.ed.ac.uk>
2026-02-03 14:37:17 +0000

    Base Model

--- dataset.py
+++ dataset.py
@@ -4,32 +4,32 @@ from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import Dataset
 
 class PendulumDataset(Dataset):
-    def __init__(self, file_paths, seq_len=5, pred_len=0):
+    def __init__(self, trajectories, seq_len=5, pred_len=0, mean=None, std=None):
         self.seq_len = seq_len
-        self.trajectories = []
+        self.pred_len = pred_len
+        self.x =[]
+        self.y = []
         
-        for path in file_paths:
-            traj = np.load(path)  # shape: (steps, 4) -> [theta1, theta2, omega1, omega2]
-            self.trajectories.append(traj)
+        if mean is None or std is None:
+            all_data = np.concatenate(trajectories, axis=0)
+            self.mean = all_data.mean(axis=0)
+            self.std  = all_data.std(axis=0) + 1e-8
+        else:
+            self.mean = mean
+            self.std  = std
         
-        all_data = np.concatenate(self.trajectories, axis=0)  # shape: (total_steps, 4)
-        self.scaler = MinMaxScaler()
-        self.scaler.fit(all_data)
-        self.trajectories = [self.scaler.transform(traj) for traj in self.trajectories]
-        
-        self.create_seq():
+        for traj in trajectories:
+            traj = (traj - self.mean) / self.std
+            for i in range(len(traj) - seq_len):
+                self.x.append(traj[i:i+seq_len])
+                self.y.append(traj[i+seq_len])
+
+        self.x = torch.tensor(self.x, dtype=torch.float32)
+        self.y = torch.tensor(self.y, dtype=torch.float32)
+
         
     def __len__(self):
         return len(self.x)
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
-    
-    def create_seq(self):
-        for traj in self.trajectories:
-            for i in range(len(traj) - seq_len):
-                self.x.append(traj[i:i+seq_len])
-                self.y.append(traj[i+seq_len:i+seq_len+pred_len])
-                
-        self.x = torch.tensor(self.x, dtype=torch.float32) 
-        self.y = torch.tensor(self.y, dtype=torch.float32) 
--- model.py
+++ model.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 import numpy as np
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers):
+    def __init__(self, input_size, hidden_size, output_size, num_layers, dropout=0.2):
         super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)#
+        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])
+        out = self.dropout(out[:, -1, :])
+        out = self.fc(out)
         return out
